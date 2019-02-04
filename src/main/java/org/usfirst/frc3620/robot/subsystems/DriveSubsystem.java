7e3a73b167d8e066287d9a259b07427ed16c70aa
Dashiel Matlock <533237@stjoebears.com>
2019-02-04 18:32:08 -0500

    Added methods to retrieve NavX Data and fixed errors with AbstractPath

--- src/main/java/org/usfirst/frc3620/robot/subsystems/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc3620/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc3620.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import org.slf4j.Logger;
+import org.usfirst.frc3620.logger.EventLogging;
+import org.usfirst.frc3620.logger.EventLogging.Level;
 import org.usfirst.frc3620.robot.RobotMap;
 import org.usfirst.frc3620.robot.commands.DriveCommand;
 
@@ -14,13 +19,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class DriveSubsystem extends Subsystem {
 
+    Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
+    public AHRS ahrs = null /* will set in the constructor */;
+
     private final DifferentialDrive differentialDrive = RobotMap.driveSubsystemDifferentialDrive;
     private Encoder driveEncoderLeft;
     private Encoder driveEncoderRight;
 
+    public double automaticHeading;
+    public boolean complainedAboutMissingAhrs;
+
     public DriveSubsystem() {
         // this code gets run when the DriveSubsystem is created 
         // (when the robot is rebooted.)
+        ahrs = new AHRS(edu.wpi.first.wpilibj.SPI.Port.kMXP);
+		ahrs.enableLogging(false);
         resetencoder();
     }
     
@@ -85,6 +98,74 @@ public class DriveSubsystem extends Subsystem {
 			right *= getSpeedModifier();
 			differentialDrive.tankDrive(left, right, false);
 		}
+	}
+
+    public boolean ahrsIsConnected() {
+		return ahrs != null && ahrs.isConnected();
+	}
+
+	void complainAboutMissingAhrs() {
+		if (!complainedAboutMissingAhrs) {
+			logger.warn("Cannot read NavX, not connected");
+		}
+		complainedAboutMissingAhrs = true;
+	}
+
+	public double getPitch() {
+		if (ahrsIsConnected()) {
+			return ahrs.getPitch();
+		} else {
+			complainAboutMissingAhrs();
+			return 0;
+		}
+	}
+
+	public double getRoll() {
+		if (ahrsIsConnected()) {
+			return ahrs.getRoll();
+		} else {
+			complainAboutMissingAhrs();
+			return 0;
+		}
+	}
+
+	public double getAngle() {
+		if (ahrsIsConnected()) {
+			return ahrs.getAngle(); //Angle direction was opposite of 2017 testing (raw is clockwise-positive, corrected is clockwise-negative)
+		} else {
+			complainAboutMissingAhrs();
+			return 0;
+		}
+    }
+
+    public double getAutomaticHeading() {
+		return automaticHeading;
+	}
+	
+	public double changeAutomaticHeading(double changeAngle) {
+		automaticHeading = automaticHeading + changeAngle;
+		automaticHeading = normalizeAngle(automaticHeading);
+		logger.info("Changing auto heading to" +  automaticHeading);
+		return automaticHeading;
+	}
+	
+	static public double normalizeAngle(double angle) {
+		// bring into range of -360..360
+		double newAngle = angle % 360;
+
+		// if it's between -360..0, put it between 0..360
+		if (newAngle < 0)
+			newAngle += 360;
+
+		return newAngle;
+	}
+	
+	public double angleDifference(double angle1, double angle2) {
+		double diff = Math.abs(angle1 - angle2);
+		if (diff > 180) {
+			diff = 360 - diff;
+		}
+		return diff;
 	}
 
     /**
