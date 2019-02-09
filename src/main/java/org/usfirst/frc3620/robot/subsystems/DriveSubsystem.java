b75777ac4867601931180d86b0fcef2fec011b2c
Doug Wegscheid <dwegscheid@sbcglobal.net>
2019-02-09 16:58:36 -0500

    Refactored.

--- src/main/java/org/usfirst/frc3620/robot/subsystems/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc3620/robot/subsystems/DriveSubsystem.java
@@ -24,7 +24,7 @@ public class DriveSubsystem extends Subsystem {
     public DriveSubsystem(){
         super();               
 		ahrs = new AHRS(edu.wpi.first.wpilibj.SPI.Port.kMXP);
-		ahrs.enableLogging(false);
+		//ahrs.enableLogging(false);
 		
         resetEncoder();
     }
