4f7ded2f1286c21c74df2794218973303a86cd85
Dashiel Matlock <533237@stjoebears.com>
2019-02-09 16:08:23 -0500

    Finished designing AutoAlignmentTemplate Receives r and theta from Chicken Vision as arguments when created and uses them to calculate X & Y and plugs them in to getMyWaypoints() so that the encoder trajectories are calculated using X & Y

--- src/main/java/org/usfirst/frc3620/robot/paths/AutoAlignmentTemplate.java
+++ src/main/java/org/usfirst/frc3620/robot/paths/AutoAlignmentTemplate.java
@@ -15,15 +15,35 @@ public class AutoAlignmentTemplate extends AbstractPath{
     }
 
     public void calculateX(){
-
+        if(angle != 0){
+            x = r*(Math.sin(angle))*(Math.sin(1.5*angle))/
+            (Math.sin(180-1.5*angle));
+        } else if(angle > 60) {
+            angle = 60;
+            x = r*(Math.sin(angle))*(Math.sin(1.5*angle))/
+            (Math.sin(180-1.5*angle));
+        } else{
+            x = 0;
+        }
     }
 
     public void calculateY(){
-
+        if(angle != 0){
+            y = r*(Math.sin(angle))*(Math.cos(1.5*angle))/
+            (Math.sin(180-1.5*angle));
+        } else if(angle > 60) {
+            angle = 60;
+            y = r*(Math.sin(angle))*(Math.cos(1.5*angle))/
+            (Math.sin(180-1.5*angle));
+        } else{
+            y = (2*r)/3;
+        }
     }
 
     @Override
     public Waypoint[] getMyWaypoints(){
+        calculateX();
+        calculateY();
         return new Waypoint[] {
             new Waypoint(0, 0, Pathfinder.d2r(0)),
             new Waypoint(x, y, Pathfinder.d2r(angle)),
