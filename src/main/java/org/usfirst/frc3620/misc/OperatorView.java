32f79ab15766e6cff7981d9e27d4c7767ee2427d
Doug Wegscheid <dwegscheid@sbcglobal.net>
2019-02-27 15:50:38 -0500

    Use new CameraServer.

--- src/main/java/org/usfirst/frc3620/misc/OperatorView.java
+++ src/main/java/org/usfirst/frc3620/misc/OperatorView.java
@@ -1,28 +1,13 @@
 package org.usfirst.frc3620.misc;
 
-
-
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
-
 import java.io.File;
 
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import edu.wpi.first.cameraserver.CameraServer;
+
 import org.slf4j.Logger;
 import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 
-/**
- * This is a demo program showing the use of OpenCV to do vision processing. The
- * image is acquired from the USB camera, then a rectangle is put on the image
- * and sent to the dashboard. OpenCV has many methods for different types of
- * processing.
- */
 public class OperatorView {
 	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
 
