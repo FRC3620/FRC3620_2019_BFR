53178bf6529916be30cc369ef00a7345c97dcd95
533237 <533237@stjoebears.com>
2019-03-27 19:13:35 -0400

    Tweaked lift setpoint for practice bot

--- src/main/java/org/usfirst/frc3620/robot/subsystems/LiftSubsystem.java
+++ src/main/java/org/usfirst/frc3620/robot/subsystems/LiftSubsystem.java
@@ -31,7 +31,7 @@ public class LiftSubsystem extends Subsystem implements PIDSource, PIDOutput {
     public static final double SETPOINT_BOTTOM = 0;
     public static final double SETPOINT_CARGO_TRASHIN = 3.25;
     public static final double SETPOINT_CARGO_CARGOSHIP = 18;
-    public static final double SETPOINT_CARGO_ROCKET_MIDDLE = 29.5;
+    public static final double SETPOINT_CARGO_ROCKET_MIDDLE = 27.5; //TODO change to 29.5 on comp
     public static final double SETPOINT_CARGO_ROCKET_TOP = 52;
 
     public static final double SETPOINT_HATCH_BOTTOM = 0;
